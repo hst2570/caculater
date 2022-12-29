011b0880bb86c8bcb624eb0e6a08268a2e98a5ca
성태 <hst2570@gmail.com>
2022-12-29 22:15:29 +0900

    아이콘 추가

--- pages/date/index.vue
+++ pages/date/index.vue
@@ -5,11 +5,26 @@
             <div class="content">
                 <div class="dateContainer">
                     <div class="date defaultDate">
-                        <label for="start">시작일</label>
+                        <label for="start">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="calendarIcon">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                                />
+                            </svg>
+                            시작일</label
+                        >
                         <CommonDateInput name="start" :value="startDate" :placeholder="'ex) 2022-01-01'" @set-value="setStartDateValue" />
                     </div>
                     <div class="date">
-                        <label for="end">종료일</label>
+                        <label for="end">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="calendarIcon">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+
+                            종료일</label
+                        >
                         <CommonDateInput name="end" :value="endDate" :placeholder="'ex) 2022-12-31'" @set-value="setEndDateValue" />
                     </div>
                 </div>
