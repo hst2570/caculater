330aa16f5eb791308737aa9658709a295edb9a8c
성태 <hst2570@gmail.com>
2023-01-11 15:50:51 +0900

    애드센스 스크립트 추가

--- app.vue
+++ app.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+    useHead({
+        title: '날짜 계산기',
+        meta: [
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: '날짜 계산을 해보세요.'
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        script: [{ async: true, crossorigin: 'anonymous', src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7317249562412325' }]
+    });
+</script>
 
 <template>
     <NuxtLayout>
